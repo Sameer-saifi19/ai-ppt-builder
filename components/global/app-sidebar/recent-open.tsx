057764a4465a36069822455bf1676e992122e6ca
Sameer <sameer.edu19@gmail.com>
2025-06-27 09:56:02 +0530

    added slideStore

--- components/global/app-sidebar/recent-open.tsx
+++ components/global/app-sidebar/recent-open.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/button'
 import { SidebarGroup, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar'
 import { Project } from '@prisma/client'
 import { JsonValue } from '@prisma/client/runtime/library'
+import { useRouter } from 'next/router'
 import React from 'react'
 import { toast } from 'sonner'
 
@@ -10,43 +11,48 @@ type Props = {
 }
 
 const RecentOpen = ({ recentProjects }: Props) => {
-    
 
-    const handleClick = (projectId: string, slides: JsonValue)  => {
-        toast.error('Project not found', {
-        description: "please try again",
-        style: {backgroundColor: 'grey'}
+    const router = useRouter()
 
-    })
-        if(!projectId || !slides){
+    const handleClick = (projectId: string, slides: JsonValue) => {
+
+        if (!projectId || !slides) {
+            toast.error('Project not found', {
+                description: "please try again",
+                style: { backgroundColor: 'grey' }
+            })
+            return
         }
+
+        setSlides(JSON.parse(JSON.stringify(slides)))
+        router.push(`/presentation/${projectId}`)
     }
-  return (
-    recentProjects.length > 0 ? ( <SidebarGroup>
-     <SidebarGroupLabel>
-        Recently Opened
-     </SidebarGroupLabel>
-     <SidebarMenu>
-       {recentProjects.length > 0 ? ( recentProjects.map((item, idx) => (
-            <SidebarMenuItem key={item.id}>
-                <SidebarMenuButton asChild tooltip={item.title} className={`hover:bg-primary-80`}>
-                    <Button variant={'link'} 
-                    onClick={()=>handleClick(item.id, item.slides)}
-                    
-                    className={`text-xs items-center justify-start`}
-                    >
-                        <span>{item.title}</span>
-                    </Button>
-                </SidebarMenuButton>
-            </SidebarMenuItem>
-        ))) : ''}
-     </SidebarMenu>
-  </SidebarGroup> ) : (
-    <Button variant={'link'}
-    onClick={() => handleClick('adssadf', '[]')}
-    >asdasd</Button>
-  )
-  )
+    return (
+        recentProjects.length > 0 ? (<SidebarGroup>
+            <SidebarGroupLabel>
+                Recently Opened
+            </SidebarGroupLabel>
+            <SidebarMenu>
+                {recentProjects.length > 0 ? (recentProjects.map((item, idx) => (
+                    <SidebarMenuItem key={item.id}>
+                        <SidebarMenuButton asChild tooltip={item.title} className={`hover:bg-primary-80`}>
+                            <Button variant={'link'}
+                                onClick={() => handleClick(item.id, item.slides)}
+
+                                className={`text-xs items-center justify-start`}
+                            >
+                                <span>{item.title}</span>
+                            </Button>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                ))) : ''}
+            </SidebarMenu>
+        </SidebarGroup>) : (
+            <Button variant={'link'}
+                onClick={() => handleClick('adssadf', '[]')}
+            >asdasd</Button>
+        )
+    )
 }
 
 export default RecentOpen
